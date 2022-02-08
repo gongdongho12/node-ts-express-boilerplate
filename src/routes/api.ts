949a51952d27b811b49484d2d3afcad1ef419e8f
Dongho Gang <gongdongho12@gmail.com>
2022-02-08 11:23:51 +0900

    feat: express 클러스터 적용 -> cpu 2개 이상일 때 멀티 워커로 멀티 프로세서 사용

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,25 +4,30 @@ import puppeteer from "puppeteer-extra";
 const router = Router();
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
-import process from 'process';
+import process from "process";
 
-const CHROMIUM_PATH = process.env.CHROMIUM_PATH
-const PUPPETEER_ARGUMENTS = { headless: true, executablePath: CHROMIUM_PATH, args: [
-  '--no-sandbox',
-  '--disable-setuid-sandbox',
-  '--disable-dev-shm-usage',
-  '--disable-gpu',
-  '--no-zygote'
-] }
+const CHROMIUM_PATH = process.env.CHROMIUM_PATH;
+const PUPPETEER_ARGUMENTS = {
+	headless: true,
+	executablePath: CHROMIUM_PATH,
+	args: [
+		"--no-sandbox",
+		"--disable-setuid-sandbox",
+		"--disable-dev-shm-usage",
+		"--disable-gpu",
+		"--no-zygote",
+	],
+};
+
+router.post("/", (req, res) => {});
 
 router.get("/", async (req, res) => {
 	const browser = await puppeteer.launch(PUPPETEER_ARGUMENTS);
 	const page = await browser.newPage();
-	const response = await page.goto(
+	console.log(req.body);
+	await page.goto(
 		"https://www.coupang.com/np/search?component=&q=%EC%82%BC%EA%B2%B9%EC%82%B4&channel=user",
-		{
-			waitUntil: "networkidle0",
-		}
+		{ waitUntil: "networkidle0" }
 	);
 	console.log(`All done, check the screenshot. ✨`);
 	const productList = await page.evaluate(() => {
@@ -46,7 +51,7 @@ router.get("/reviews", async (req, res) => {
 	});
 	const reviewPageUrlGenerator = (page: number) => {
 		return `https://www.coupang.com/vp/product/reviews?productId=${productId}&page=${page}&size=30&sortBy=DATE_DESC&ratings=&q=&viRoleCode=3&ratingSummary=true`;
-	}
+	};
 	const reviewRequestGenerator = (pageNumber: number) => {
 		const resultBody: Promise<string> = page.evaluate(
 			({ subRequestUrl }) => {
@@ -64,11 +69,16 @@ router.get("/reviews", async (req, res) => {
 			{ subRequestUrl: reviewPageUrlGenerator(pageNumber) }
 		);
 		return resultBody;
-	}
+	};
 	const pageNumberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	const reviewPagePromiseList = pageNumberList.map(reviewRequestGenerator);
-	const resultReviewList: string[] = await Promise.all(reviewPagePromiseList)
-	const result = new Map(pageNumberList.map((pageNumber, i) => [reviewPageUrlGenerator(pageNumber), resultReviewList[i]]))
+	const resultReviewList: string[] = await Promise.all(reviewPagePromiseList);
+	const result = new Map(
+		pageNumberList.map((pageNumber, i) => [
+			reviewPageUrlGenerator(pageNumber),
+			resultReviewList[i],
+		])
+	);
 	browser.close();
 	res.send(Object.fromEntries(result));
 });
